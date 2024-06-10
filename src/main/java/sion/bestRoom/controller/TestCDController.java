53771242810546224e20f0d3d4b948560176baee
rlatldhs32 <sion_kim@tmax.co.kr>
2024-06-10 17:19:05 +0900

    feat : 업로드용 테스트 6

--- src/main/java/sion/bestRoom/controller/TestCDController.java
+++ src/main/java/sion/bestRoom/controller/TestCDController.java
@@ -2,13 +2,12 @@ package sion.bestRoom.controller;//package sion.bestRoom.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class TestCDController {
 
     @GetMapping("/test")
     public String getDabangRooms() throws InterruptedException {
